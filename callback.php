4d165f71feb0128e03cf6e31dee20f5ee80dc409
Charlespura <charles051902pura@gmail.com>
2025-06-07 20:13:43 +0800

    try and tr

--- callback.php
+++ callback.php
@@ -2,7 +2,7 @@
 // Spotify app credentials
 $client_id = '47ca30ec497f4e0eb84dde015e6fa71e';
 $client_secret = '717778de811f483b9c6acd2712a24173';
-$redirect_uri = 'https://cpportfolio.onrender.com/callback';
+$redirect_uri = 'https://cpportfolio.onrender.com/callback.php';
 
 // Get the authorization code from the URL
 $code = $_GET['code'];
@@ -28,7 +28,8 @@ curl_close($ch);
 $data = json_decode($response, true);
 if (isset($data['access_token'])) {
     // Save refresh token & access token securely
-    file_put_contents('spotify_tokens.json', json_encode($data));
+  file_put_contents(__DIR__ . '/token/spotify_tokens.json', json_encode($data));
+
 
     echo "Authorization successful! Refresh token saved.";
 } else {
